b969449efbcf9f4a1e8e111f3a4f2786a46b5cb5
angel silaban <magdalenangela20@gmail.com>
2022-03-12 16:09:29 +0700

    Kumpul Jurnal 03

---
+++ Node.java
@@ -0,0 +1,12 @@
+package company;
+
+public class Node<E> { E data;
+    Node<E> next;
+
+    public Node(E object){ data = object; this.next = null;
+    }
+
+    public E getData(){
+        return data;
+    }
+}
